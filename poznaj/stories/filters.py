eb919d3c7dafb070ca5d2760eca0eafd6574aef2
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-03-05 15:36:58 +0100

    Filter list of stories by lat/long. Closes #4

---
+++ poznaj/stories/filters.py
@@ -0,0 +1,32 @@
+from django.contrib.gis.geos import GEOSGeometry
+from rest_framework import filters
+from rest_framework.exceptions import ValidationError
+
+
+WRONG_LAT_LONG_TEXT = 'Provide float for latitude and longitude'
+
+
+class FirstPointFilter(filters.BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        lat = request.query_params.get('lat', None)
+        long = request.query_params.get('long', None)
+        if lat and long:
+            point = self.create_point(lat, long)
+            return queryset.distance(point, field_name='first_point__geom').order_by('distance')
+
+        return queryset
+
+    @staticmethod
+    def create_point(latitude, longitude):
+        try:
+            point = GEOSGeometry(
+                'POINT({long} {lat})'.format(long=longitude, lat=latitude), srid=4326
+            )
+        except ValueError:
+            raise ValidationError(WRONG_LAT_LONG_TEXT)
+
+        return point
+
+    def to_html(self, request, queryset, view):
+        return 'To order by distance use ?lat=10&long10 in url'
--- poznaj/stories/views.py
+++ poznaj/stories/views.py
@@ -2,9 +2,12 @@ from rest_framework import permissions, viewsets
 
 from .models import Story
 from .serializers import StorySerializer
+from .filters import FirstPointFilter
 
 
 class StoriesViewSet(viewsets.ModelViewSet):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    filter_backends = (FirstPointFilter,)
+    ordering_fields = ('first_point',)
