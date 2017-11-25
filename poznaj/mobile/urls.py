0dbe7f9e45b329317986cef6d666e439169c674d
Rafał Gawlik <rafal.gawlik@gmail.com>
2017-11-25 20:29:40 +0100

    Add rest manu pages, add main page

--- poznaj/mobile/urls.py
+++ poznaj/mobile/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import TeamView, PartnersView
+from .views import AboutView, ClubView, TeamView, PartnersView
 
 urlpatterns = [
+    url(r'^about/$', AboutView.as_view()),
+    url(r'^club/$', ClubView.as_view()),
     url(r'^team/$', TeamView.as_view()),
     url(r'^partners/$', PartnersView.as_view()),
 ]
--- poznaj/mobile/views.py
+++ poznaj/mobile/views.py
@@ -1,9 +1,13 @@
 from django.views.generic import TemplateView
 
+class AboutView(TemplateView):
+    template_name = 'mobile/about.html'
+
+class ClubView(TemplateView):
+    template_name = 'mobile/club.html'
 
 class TeamView(TemplateView):
     template_name = 'mobile/team.html'
-
 
 class PartnersView(TemplateView):
     template_name = 'mobile/partners.html'
